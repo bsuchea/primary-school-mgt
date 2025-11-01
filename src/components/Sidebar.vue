33c0e414a589241575f5cb03b734876172200e6d
suchea <bsuchea@gmail.com>
2025-11-01 10:09:59 +0700

    new lable menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -44,7 +44,7 @@
                         d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
                       </path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">Kanban</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Score</span>
                     <span
                       class="bg-gray-200 text-gray-800 ml-3 text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">Pro</span>
                   </a>
@@ -61,9 +61,31 @@
                         d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
                       </path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">Inbox</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Attendent</span>
                     <span
                       class="bg-gray-200 text-gray-800 ml-3 text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">Pro</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#"
+                    class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                      fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Student</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#"
+                    class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                      fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Teacher</span>
                   </a>
                 </li>
                 <li>
@@ -86,7 +108,7 @@
                         d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
                         clip-rule="evenodd"></path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">Products</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Subject</span>
                   </a>
                 </li>
                 <li>
@@ -98,7 +120,7 @@
                         d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
                         clip-rule="evenodd"></path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">Sign In</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">Class</span>
                   </a>
                 </li>
                 <li>
