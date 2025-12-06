2f64de33b79f1a9c3642fd24388e8fb4d14f5f48
suchea <bsuchea@gmail.com>
2025-12-06 07:55:34 +0700

    set page and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Dashboard from '@/views/Dashboard.vue'
+import Product from '@/views/product/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'dashboard',
       component: Dashboard,
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: Product,
     },
     // {
     //   path: '/about',
